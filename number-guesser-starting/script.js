81edaf388a9bb825ef65da6df35fd9a899f8bb64
neetmangat <neetmangat@gmail.com>
2020-09-04 04:58:09 -0500

    Finished Number Guesser

--- number-guesser-starting/script.js
+++ number-guesser-starting/script.js
@@ -3,4 +3,35 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+const generateTarget = () => {
+    return Math.floor(Math.random() * 10)
+}
 
+const compareGuesses = (userGuess, computerGuess, secretTarget) => {
+    const userDifference = Math.abs(secretTarget - userGuess);
+    const computerDifference = Math.abs(secretTarget - computerGuess);
+
+    if (userDifference === computerDifference) {
+        return true;
+    }
+
+    if (userDifference > computerDifference) {
+        return false;
+    }
+
+    if (userDifference < computerDifference) {
+        return true;
+    }
+}
+
+const updateScore = winner => {
+    if (winner === 'human') {
+        humanScore += 1;
+    } else if (winner === 'computer') {
+        computerScore += 1;
+    }
+}
+
+const advanceRound = () => {
+    currentRoundNumber += 1
+}
